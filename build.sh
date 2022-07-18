95e8acc0055307814bd4b0dd1bcf3ff818e4d9e5
Tavis Ormandy <taviso@gmail.com>
2022-07-17 21:43:54 -0700

    enforce script perms

--- build.sh
+++ build.sh
@@ -52,6 +52,8 @@ find "${pkgname}" -type d -exec chmod 0755 {} \;
 cp control "${pkgname}/DEBIAN/control"
 cp wp.sh "${pkgname}/usr/bin/wp"
 
+chmod 0755 "${pkgname}/usr/bin/wp"
+
 # Fetch any missing distfiles.
 test -f "${libcurl##*/}" || wget "${libcurl}"
 test -f "${ldsourl##*/}" || wget "${ldsourl}"
